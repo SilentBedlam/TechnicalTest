8b04e6fd4fe31f68e8510544b408e3d4d14935c4
Adam Herrick <neutron619@yahoo.co.uk>
2020-12-07 09:16:08 +0000

    Work on UI.

--- Bds.TechTest/wwwroot/js/app.js
+++ Bds.TechTest/wwwroot/js/app.js
@@ -1,21 +1,66 @@
 ﻿$(document).ready(function () {
+    // Creates the HTML for a single search result.
+    var createResultRowHtml = function (searchResult) {
+        let html = '<div class="row">';
+        html += '<a href="' + searchResult.uri + '">';
+        html += '<h4>' + searchResult.pageTitle + '<small class="text-muted">' + searchResult.uri + '</small></h4>';
+        html += '</a></div>';
+        return html;
+        //// "searchResults":[{"pageTitle":"Vitamin D - Health Professional Fact Sheet","uri":"https://ods.od.nih.gov/factsheets/VitaminD-HealthProfessional/","averageRank":1,"rawSearchResults":[{"providerName":"Google","rank":1,"pageTitle":"Vitamin D - Health Professional Fact Sheet","uri":"https://ods.od.nih.gov/factsheets/VitaminD-HealthProfessional/"}]}
+    };
 
-    // Handle the search button's click event: submit a request to the server and retrieve the data from multiple search engines.
-    $('#search').on("click", function () {
-        // TBC: Add validation of the input.
-        let searchTerm = $('#searchTerm').val();
+    // Resets the form to it's original appearance.
+    var resetForm = function () {
+        $("#searchTerm").val("");
+        $("#searchResults, #searchError, #spinner").hide();
+        $("#splash").show();
+    };
 
-        $.ajax({
-            url: "./api/CombinedSearch",
-            contentType: "application/json",
-            method: "POST",            
-            data: JSON.stringify({ "SearchTerm": searchTerm }),
-            error: function () {
-                alert("Failed");
-            },
-            success: function (result) {
-                alert(JSON.stringify(result));
-            }
-        })
-    })
+    // Handles the results of a successful search.
+    var handleSearchSuccess = function (searchResults) {
+        // Hide the spash screen but keep the spinner until we've finished processing.
+        $("#splash").hide();
+
+        if (searchResults.searchResults) {
+            let resultsHtml = "";
+            searchResults["searchResults"].forEach(function (value) {
+                let singleResultHtml = createResultRowHtml(value);
+                resultsHtml += singleResultHtml;
+            });
+            $("#searchResultsPlaceholder").append(resultsHtml);
+        }        
+
+        $("#spinner").hide();
+        $("#searchResults").show();
+    };
+
+    // Handles the results of a failed search.
+    var handleSearchError = function () {
+        $("#splash, #spinner").hide();
+        $("#searchError").show();
+    };
+
+    // Handle the search button"s click event: submit a request to the server and retrieve the data from multiple search engines.
+    $("#search").on("click", function () {
+        $("#spinner").show();
+
+        let searchTerm = $("#searchTerm").val().trim();
+
+        if (searchTerm) {
+            $.ajax({
+                url: "./api/CombinedSearch",
+                contentType: "application/json",
+                method: "POST",
+                data: JSON.stringify({ "SearchTerm": searchTerm }),
+                error: handleSearchError,
+                success: handleSearchSuccess
+            });
+        } else {
+            alert("A search term must be provided and may not be composed entirely of white space.");
+        }
+    });
+
+    // Handle the reset link's click event.
+    // Reset the form by clearing the search text, and showing the page as it originally appeared.
+    $("#resetFormLink1, #resetFormLink2").on("click", resetForm);
 });
